6fb741b7975d5d64f4f6623e2dece03f0b513060
LucasCodes01 <lucascodesweb@gmail.com>
2022-09-17 19:35:09 +1200

    Added part 2.1 source code

---
+++ Part 2.1/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: GatewayIntentBits.Guilds });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    command.execute(interaction);
+  } catch (err) {
+    console.error(err);
+    interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+  }
+});
+
+client.login(process.env.TOKEN);
