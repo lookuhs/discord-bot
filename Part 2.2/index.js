d768f3893c1859502b1240eaacc67f4c71fa3b0e
LucasCodes01 <lucascodesweb@gmail.com>
2022-09-24 09:06:13 +1200

    Added part 2.2

---
+++ Part 2.2/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
+const { registerEvents, registerCommands } = require('./utils');
+
+registerEvents(client);
+registerCommands(client);
+
+
+client.login(process.env.TOKEN);
